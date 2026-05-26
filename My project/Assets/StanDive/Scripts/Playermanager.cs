f8fa6a15f7d78fa621a35d8ad347e632754938f6
Yolanda1112 <231205033@ccmailg.meijo-u.ac.jp>
2026-05-26 17:12:32 +0900

    dive

---
+++ My project/Assets/StanDive/Scripts/Playermanager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Playermanager : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float moveSpeed = 10f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        // キーボードがPCに認識されているか安全のためにチェック
+        if (Keyboard.current == null) return;
+
+        //右に進む
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.AddForce(transform.right * moveSpeed);
+        }
+        //左に進む
+        if (Keyboard.current.aKey.isPressed)
+        {
+            rb.AddForce(-transform.right * moveSpeed);
+        }
+    }
+
+
+}
