69af09c01229c59fa528f2f9bf66f8174c0899a9
Kazuki Shirai <147134328+shirai-kazuki@users.noreply.github.com>
2026-07-06 15:24:47 +0900

    ab

--- My project/Assets/StanDive/Scripts/RingNPCmanager.cs
+++ My project/Assets/StanDive/Scripts/RingNPCmanager.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RingNPCmanager : MonoBehaviour
 {
     public Transform target; // 目的地のTransformを保存する変数
-    private float speed = 10f;
+    private float speed = 100f;
+    private float arrivalThreshold = 0.1f; // 到達とみなす距離
+    private Quaternion startRotation;
+    // 目標の角度
+        Quaternion targetRotation;
+    // 回転するスピード
+    public float rotationSpeed = 5.0f; 
 
     void Start()
     {
-        
+        // ゲーム開始時の初期姿勢を記憶しておく
+        startRotation = transform.rotation;
     }
 
     void Update()
@@ -20,6 +28,28 @@ public class RingNPCmanager : MonoBehaviour
             // 移動処理
             float step = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+
+            // 現在地と目的地の距離を計算
+            float distance = Vector3.Distance(transform.position, target.position);
+
+            // 距離が閾値以下になったら到着とする
+            if (distance <= arrivalThreshold)
+            {
+                // 初期姿勢
+                targetRotation = startRotation;
+            }
+            else
+            {
+                // 初期姿勢からY軸だけ90度傾いた状態
+                targetRotation = startRotation * Quaternion.Euler(90, 0, 0);
+            }
+
+            // 現在の角度から、目標の角度へ「徐々に」近づける
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation, 
+                targetRotation, 
+                Time.deltaTime * rotationSpeed
+            );
         }
     }
 }
