c4ecc4394261a641ee0c6cbc771819c44c7f88e1
shirai-kazuki <231205100@ccmailg.meijo-u.ac.jp>
2026-09-03 18:48:35 +0900

    景色

--- My project/Assets/StanDive/Scripts/Playermanager.cs
+++ My project/Assets/StanDive/Scripts/Playermanager.cs
@@ -222,13 +222,9 @@ public class Playermanager : MonoBehaviour
         {
             ringController.ChangePSkybox(0);
         }
-        if (transform.position.y < 300f && isParachute)
+        if (transform.position.y < 250f && isParachute)
         {
             ringController.ChangePSkybox(4);
-        }
-        if (transform.position.y < 150f && isParachute)
-        {
-            ringController.ChangePSkybox(5);
         }
 
 
