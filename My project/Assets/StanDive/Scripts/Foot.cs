cfcb518c6eec063404862c23d0500cd736ffb371
yuto272 <231205162@ccmailg.meijo-u.ac.jp>
2026-09-21 14:26:04 +0900

    ハード修正

--- My project/Assets/StanDive/Scripts/Foot.cs
+++ My project/Assets/StanDive/Scripts/Foot.cs
@@ -5,12 +5,6 @@ public class Foot : MonoBehaviour
 {
     private SerialPort sp;
 
-    // Vp4 状態
-    private bool vp4On = false;
-
-    // HAPTIC REACTOR 状態
-    private bool hapticOn = false;
-
     void Start()
     {
         sp = new SerialPort("COM5", 115200);
@@ -24,18 +18,12 @@ public class Foot : MonoBehaviour
 
     public void startFoot()
     {
-        vp4On = true;
-        hapticOn = true;
-
         SendCommand("BackOn");
         SendCommand("HapticOn");
     }
 
     public void stopFoot()
     {
-        vp4On = false;
-        hapticOn = false;
-
         SendCommand("BackOff");
         SendCommand("HapticOff");
     }
